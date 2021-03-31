d0c274560f577c29421896eeb52cf8a06fe7bc88
Andrew Morris <ajm1230b@gmail.com>
2021-03-31 15:17:47 -0400

    Create Blackjack.java
    Brielfy initialized the file and started structuring the BASE for the game; i will steadily implement these classes throughout today/the next few days

---
+++ Blackjack.java
@@ -0,0 +1,39 @@
+// Andrew Morris, Oksana Rubis, Tommy Chang
+//Blackjack game
+// Project X
+
+import java.util.Random;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Blackjack{
+
+    public void main(String args[])
+    {
+
+    }   
+}
+
+class dealer{ 
+
+}
+
+class player{
+
+}
+
+class deck{
+    private card[] cardDeck;
+
+    class card{
+        private Random rand = new Random();
+        private int suit;
+        private int num;
+
+        
+
+    }
+}   
