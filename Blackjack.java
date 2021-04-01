42efd9b0d263048252a009d78298b00f90a7c2aa
Andrew Morris <ajm1230b@gmail.com>
2021-03-31 23:40:46 -0400

    Worked more on implementing the base
    Worked on classes for base game

--- Blackjack.java
+++ Blackjack.java
@@ -26,14 +26,63 @@ class player{
 }
 
 class deck{
-    private card[] cardDeck;
+    private Random rand = new Random();
+    private card[] deck;
+
+    public void shuffle(){
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int j = 0; j < 52; ++j){
+                int r = j + rand.nextInt(52 - j);
+
+                card temp = deck[r];
+                deck[j] = deck[r];
+                deck[r] = temp;
+            }
+        }
+    }
 
     class card{
-        private Random rand = new Random();
-        private int suit;
-        private int num;
-
+        private int cardValue;
+        private int suitValue;
+        private String cardSuit;
+        private String specCard;
         
+        public void setCard(){  
+            //Create the cards for our deck;
+            while(deck.length != 51){
+                cardValue = rand.nextInt(4);                                            // Generate a random value 1-4 for the card face;
+                suitValue = rand.nextInt((13));                                         // generate a random value 2-10 for the card value;
+                card newCard = new card();                                              // Create a card object to hold the information in the card
+
+                if(cardValue < 2 || cardValue > 10)
+                {
+                    if (cardValue == 1)
+                    {
+                        newCard.specCard = "Ace";
+                    }
+                    else if (cardValue > 10)
+                    {
+                        switch(cardValue){
+                            case 11 : {newCard.specCard = "Jack"; newCard.cardValue = 10;}
+                            case 12 : {newCard.specCard = "Queen"; newCard.cardValue = 10;}
+                            case 13 : {newCard.specCard = "King"; newCard.cardValue = 10;}
+                        }
+                    }
+                }
+                else
+                    newCard.cardValue = cardValue;
+
+                switch(suitValue){
+                    case 1 : {cardSuit = "Clubs"; newCard.cardSuit = cardSuit;}
+                    case 2 : {cardSuit = "Diamonds"; newCard.cardSuit = cardSuit;}
+                    case 3 : {cardSuit = "Hearts"; newCard.cardSuit = cardSuit;}
+                    case 4 : {cardSuit = "Spades"; newCard.cardSuit = cardSuit;}
+                }
+
+
+            }
+        }
 
     }
 }   
