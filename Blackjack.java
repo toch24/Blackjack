8480473b81b9b15dd2940d265cd89368cfdcbd5e
Andrew Morris <ajm1230b@gmail.com>
2021-04-01 14:02:40 -0400

    Debugging deck class still
    Made a lot of changes to the deck class..

--- Blackjack.java
+++ Blackjack.java
@@ -1,21 +1,19 @@
 // Andrew Morris, Oksana Rubis, Tommy Chang
-//Blackjack game
+// Blackjack game
 // Project X
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Blackjack{
 
     public static void main(String args[])
     {
-        deck gameDeck = new deck();                 // testing
-        gameDeck.populateDeck();
+        deck temporary = new deck();
+        temporary.populateDeck();
     }   
 }
 
@@ -29,28 +27,27 @@ class player{
 
     }
 
-    public void fillHand(card getCard){
+    public void fillHand(deck.card getCard){
 
     }
 }
 
 class deck{
-    private card[] deck = new card[52];
+    private card[] gameDeck = new card[52];
     private Random rand = new Random();
 
+
     public void populateDeck(){
-        int counter = 0;                // Create a counter so we can fill the entire deck without the use of an interator for the sake of runtime
+        int counter = 0;
+        card temp = new card();
 
-        while (counter != 52)
-        {   // Fill the deck with 52 cards (The entire deck)
-            card newCard = new card();                          //Create card variable
-            newCard.setCards(newCard);                          // Call the setCard function to populate this card
+        temp.setCards(gameDeck);
 
-            deck[counter] = newCard;
-            System.out.print("\nThis is whats in the deck at counter: " + counter + " " + deck[counter].getValue(deck[counter]) + " " + deck[counter].getSuit(deck[counter]));
-            if(deck[counter].getSpec(deck[counter]) != null)
+        while(counter != gameDeck.length){
+            System.out.print("\nThis is whats in the gameDeck at counter: " + counter + " " + gameDeck[counter].getValue(gameDeck[counter]) + " " + gameDeck[counter].getSuit(gameDeck[counter]));
+            if(gameDeck[counter].getSpec(gameDeck[counter]) != null)
             {
-                System.out.print(" " + deck[counter].getSpec(deck[counter]));
+                System.out.print(" " + gameDeck[counter].getSpec(gameDeck[counter]));
             }
             counter++;
         }
@@ -62,103 +59,74 @@ class deck{
             for (int j = 0; j < 52; ++j){
                 int r = j + rand.nextInt(52 - j);
 
-                card temp = deck[r];
-                deck[j] = deck[r];
-                deck[r] = temp;
+                card temp = gameDeck[r];
+                gameDeck[j] = gameDeck[r];
+                gameDeck[r] = temp;
             }
         }
     }
-}
 
-class card{
-    private int cardValue;
-    private int suitValue;
-    private String cardSuit;
-    private String specCard;
-    private Random rand = new Random();
-    private ArrayList<card> poppedCards = new ArrayList<card>(52);
-
-    public void setCards(card newCard){  
-        //Create the cards for our deck;
-            
-            cardValue = rand.nextInt((13 - 1) + 1);                                          // Generate a random value 1-4 for the card face;
-            suitValue = rand.nextInt((4 - 1) + 1);                                         // generate a random value 2-10 for the card value;
-            
-            if(cardValue == 0){
-                while(cardValue == 0){
-                    cardValue = rand.nextInt((13 - 1) + 1);                                          // Generate a random value 1-4 for the card face;
-                }
-            }
-            if(suitValue == 0){
-                while(suitValue == 0){
-                    suitValue = rand.nextInt((4 - 1) + 1);                                          // Generate a random value 1-4 for the card face;
-                }
-            }
-
-            if(cardValue < 2 || cardValue > 10)
-            {
-                if (cardValue == 1)
+    class card{
+        private int cardValue;
+        private String cardSuit;
+        private String specCard;
+    
+        public card[] setCards(card newCard[]){  
+            for (int i = 0; i < 4; ++i)
+                for (int j = 0; j < 13; ++j)
                 {
-                    newCard.specCard = "Ace";
-                    newCard.cardValue = 1;
-                }
-                else if (cardValue > 10)
-                {
-                    switch(cardValue){
-                        case 11 : {newCard.specCard = "Jack"; newCard.cardValue = 10;}
-                        case 12 : {newCard.specCard = "Queen"; newCard.cardValue = 10;}
-                        case 13 : {newCard.specCard = "King"; newCard.cardValue = 10;}
+                    if(i == 0)
+                        newCard[j].cardSuit = "Hearts";
+                    else if(i == 1)
+                        newCard[j].cardSuit = "Spades";
+                    else if(i == 2)
+                        newCard[j].cardSuit = "Clubs";
+                    else if(i == 3)
+                        newCard[j].cardSuit = "Diamonds";
+                    
+                    if(j == 0)
+                    {
+                        newCard[j].cardValue = 1;
+                        newCard[j].specCard = "Ace";
+                    }
+                    else if(j > 0 && j < 9)
+                    {
+                        newCard[j].cardValue = j+1;
+                    }
+                    else if(j >= 9)
+                    {
+                        newCard[j].cardValue = 10;
+                        if(j == 10){
+                            newCard[j].cardValue = 10;
+                            newCard[j].specCard = "Jack";
+                        }
+                        else if(j == 11){
+                            newCard[j].cardValue = 10;
+                            newCard[j].specCard = "Queen";
+                        }
+                        else if(j == 12){
+                            newCard[j].cardValue = 10;
+                            newCard[j].specCard = "King";
+                        }
                     }
                 }
+                return newCard;
             }
-            else
-                newCard.cardValue = cardValue;
-
-                if(suitValue > 0){
-                    switch(suitValue){
-                        case 1 : {newCard.cardSuit = "Clubs";}
-                        case 2 : {newCard.cardSuit = "Diamonds";}
-                        case 3 : {newCard.cardSuit = "Hearts";}
-                        case 4 : {newCard.cardSuit = "Spades";}
-                    }
-                }
-                
-            if(!poppedCards.isEmpty()){
-                if(!checkCardEqualsTo(newCard)){
-                    poppedCards.add(newCard);
-                }
-            }
-            else
-                poppedCards.add(newCard);
+    
+        public int getValue(card passed){
+            return passed.cardValue;
         }
-
-    public int getValue(card passed){
-        return passed.cardValue;
-    }
-
-    public String getSuit(card passed){
-        return passed.cardSuit;
-    }
-
-    public String getSpec(card passed){
-        if(specCard != null)
-            return passed.specCard;
-        else
-            return null;    
-    }
-
-    public boolean checkCardEqualsTo(card Check){                                       // This function will be used to make sure we dont put two of the same cards in the deck
-        for (int i = 0; i < poppedCards.size() + 1; ++i){
-            if(poppedCards.get(i).cardSuit == Check.cardSuit){
-                if(poppedCards.get(i).cardValue == Check.cardValue)
-                    return true;
-            }
-            else if(poppedCards.get(i).cardSuit == Check.cardSuit)
-                if(poppedCards.get(i).specCard == Check.specCard){
-                    return true;
-                }
-            else return false;
+    
+        public String getSuit(card passed){
+            return passed.cardSuit;
         }
-        return false;
-    }
+    
+        public String getSpec(card passed){
+            if(specCard != null)
+                return passed.specCard;
+            else
+                return null;    
+        }
+    }    
 }
+
