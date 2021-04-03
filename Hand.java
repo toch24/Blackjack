c85557466dba319ecf27b7060a71daca27d45607
Tommy Chong <tc18cj@my.fsu.edu>
2021-04-02 20:26:42 -0400

    Change amount of cards in hand from 2 to 8

--- Hand.java
+++ Hand.java
@@ -1,6 +1,6 @@
 public class Hand {
-    //2 cards in each hand (will need to create a hand instance for each player including the dealer)
-    private Card[] hand = new Card[2];
+    //Theoretically we dont need more than 8 cards in hand...?
+    private Card[] hand = new Card[8];
 	private int count = 0;
     
     public void addCard(Card card) {
@@ -15,9 +15,35 @@ public class Hand {
 	
 	public int handPeek() {
 
-        //returns the value of the second card in hand
+        //Peek the card that is face down
 		int value = hand[1].getcardValue();
 		return value;
 	}
 
+
+    //Calculate the total value of cards. This also checks to see whether to use 1 or 11 for ace
+	public int total() {
+        boolean ace = false;
+		int total =0;
+		
+		for (int i = 0; i < count; i++) {
+			int value = hand[i].getcardValue();
+            
+            //if card value is 10, Jack, Queen or King
+			if (value >= 10) {
+				value = 10;
+			} else if ( value == 1) {
+				ace = true;
+			}
+            //add value to the total
+			total += value;
+		}
+
+        //Add 10 to the total if there is an ace and the total with 11 is equal to or less than 21
+		if (ace && (total + 10 <= 21)) {
+			total += 10;
+		}
+
+		return total;
+	}
 }
