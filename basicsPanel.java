f372dc340f386b1ff0081c7e6a633a904821db8f
orubis <70862576+orubis@users.noreply.github.com>
2021-04-07 15:55:00 -0400

    Adds bet button to basicsPanel

--- basicsPanel.java
+++ basicsPanel.java
@@ -12,12 +12,20 @@ import javax.swing.border.Border;
 import javax.swing.text.AbstractDocument.Content;
 
 public class basicsPanel extends JPanel{
-    @Override
+    //@Override
+
+  private JButton bet;
+    public basicsPanel(){
+      bet = new JButton("Bet");
+      bet.setBounds(350, 400,100, 100);
+      add(bet);
+    }
+
     public void paintComponent( Graphics g )
     {
-        g.setColor(Color.white);  
+        g.setColor(Color.white);
         g.fillRect(280, 360 , 100, 150);
-    
+
         g.fillRect(400, 360 , 100, 150);
 
     }
