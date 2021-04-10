e33967ca2111f2afad7057e218c6997f832e0cf6
orubis <70862576+orubis@users.noreply.github.com>
2021-04-10 19:41:34 -0400

    Showing front side of cards for bots when round ends

--- basicsPanel.java
+++ basicsPanel.java
@@ -29,7 +29,7 @@ public class basicsPanel extends JPanel{
 
     public basicsPanel(){
       game = new Blackjack();
-      setCards();                                   //sets the cards images
+      setCards(1);                                   //sets the cards images
 
       //setting layout to null, default layout is flow layout
       setLayout(null);
@@ -55,8 +55,9 @@ public class basicsPanel extends JPanel{
       add(startGame);
 
       highestbetlabel = new JLabel("Highest bet: " + String.valueOf(Pot.getHighestBet()));
-      highestbetlabel.setBounds(470,250,100,50);
+      highestbetlabel.setBounds(350,150,200,50);
       add(highestbetlabel);
+
 
       startGame.addActionListener(new ActionListener(){
         public void actionPerformed(ActionEvent e) {
@@ -150,7 +151,7 @@ public class basicsPanel extends JPanel{
           String msg = "Card drawn!";
           JOptionPane.showMessageDialog(null, msg);
           //update the set of cards for player
-          setCards();
+          setCards(1);
 
       }
     });
@@ -168,7 +169,7 @@ public class basicsPanel extends JPanel{
       Thread.sleep(10000);
       boolean play = game.play(players);
       if(play){
-        String msg = "Player 1 played";               
+        String msg = "Player 1 played";
         JOptionPane.showMessageDialog(null, msg);
         highestbetlabel.setText("Highest bet: " + String.valueOf(Pot.getHighestBet()));
         players++;
@@ -176,12 +177,12 @@ public class basicsPanel extends JPanel{
       else System.out.println("Computer player 1 is having some problems...");
         }
         catch(Exception e){
-          
+
         }
       }
       //Enable buttons for human player once first bot plays
       if(players == 2){
-        String msg = "Your turn!";               
+        String msg = "Your turn!";
         JOptionPane.showMessageDialog(null, msg);
         bet.setEnabled(true);
         hit.setEnabled(true);
@@ -196,11 +197,11 @@ public class basicsPanel extends JPanel{
       pass.setEnabled(false);
       boolean play = game.play(players);
       if(play){
-        String msg = "Player 2 played";               
+        String msg = "Player 2 played";
         JOptionPane.showMessageDialog(null, msg);
         highestbetlabel.setText("Highest bet: " + String.valueOf(Pot.getHighestBet()));
         players++;
- 
+
       }
       else System.out.println("Computer player 2 is having some problems...");
     }
@@ -215,14 +216,14 @@ public class basicsPanel extends JPanel{
         bet.setEnabled(false);
         hit.setEnabled(false);
         pass.setEnabled(false);
-        
+
         boolean play = game.play(players);
         if(play){
-          String msg = "Dealer played";               
+          String msg = "Dealer played";
           JOptionPane.showMessageDialog(null, msg);
+          setCards(2);
           highestbetlabel.setText("Highest bet: " + String.valueOf(Pot.getHighestBet()));
           players = 1;
-      
         }
         else System.out.println("Dealer is having some problems...");
       }
@@ -236,11 +237,11 @@ public class basicsPanel extends JPanel{
     }
 
 
-    public void setCards(){
+    public void setCards(int n){
       int cardvalue;
       String suit;
       int pos = 250;
-
+      if(n ==1){
       cards = game.getPlayerCards();
       for(Card c : cards){
         //If the card is not null, then get all the data. (Need this error checking because Card array is initialized with size of 14)
@@ -326,8 +327,8 @@ public class basicsPanel extends JPanel{
         }
 
       }   //end of if statement for c != null
-      }   //end of for loop for iteration of each card in hand
-
+    }   //end of for loop for iteration of each card in hand
+}
 
         //set bot cards
 
@@ -347,11 +348,69 @@ public class basicsPanel extends JPanel{
           String png = "";                                     //name of the png file
           String value = String.valueOf(cardvalue);
 
+          if(c.getspecCard() == null)
+            {
+              if(suit.equals("Hearts")){
+                png = "Cards/" + value + "H" + ".png";
+              }
+              else if(suit.equals("Spades")){
+                png = "Cards/" + value + "S" + ".png";
+              }
+              else if(suit.equals("Clubs")){
+                png = "Cards/" + value + "C" + ".png";
+              }
+              else if(suit.equals("Diamonds")){
+                png = "Cards/" + value + "D" + ".png";
+              }
+            }
+            else if(c.getspecCard() != null)        // we are checking if we have a face card :)
+            { // This whole switch statement accounts for if the card has a face (I.E] Jack,Queen,King)
+              if(c.getspecCard() == "King"){
+                switch(suit){
+                  case "Hearts" : {png = "Cards/" + 13 + "H" + ".png";}
+                  case "Clubs"  : {png = "Cards/" + 13 + "C" + ".png";}
+                  case "Spades"  : {png = "Cards/" + 13 + "S" + ".png";}
+                  case "Diamonds"  : {png = "Cards/" + 13 + "D" + ".png";}
+                }
+              }
+              else if(c.getspecCard() == "Queen"){
+                switch(suit){
+                  case "Hearts" : {png = "Cards/" + 12 + "H" + ".png";}
+                  case "Clubs"  : {png = "Cards/" + 12 + "C" + ".png";}
+                  case "Spades"  : {png = "Cards/" + 12 + "S" + ".png";}
+                  case "Diamonds"  : {png = "Cards/" + 12 + "D" + ".png";}
+                }
+              }
+              else if(c.getspecCard() == "Jack"){
+                switch(suit){
+                  case "Hearts" : {png = "Cards/" + 11 + "H" + ".png";}
+                  case "Clubs"  : {png = "Cards/" + 11 + "C" + ".png";}
+                  case "Spades"  : {png = "Cards/" + 11 + "S" + ".png";}
+                  case "Diamonds"  : {png = "Cards/" + 11 + "D" + ".png";}
+                }
+              }
+              else if(c.getspecCard() == "Ace"){
+                switch(suit){
+                  case "Hearts" : {png = "Cards/" + 1 + "H" + ".png";}
+                  case "Clubs"  : {png = "Cards/" + 1 + "C" + ".png";}
+                  case "Spades"  : {png = "Cards/" + 1 + "S" + ".png";}
+                  case "Diamonds"  : {png = "Cards/" + 1 + "D" + ".png";}
+                }
+              }
+            }
+
+
           //for bot 1 and bot 2, just show the amount of cards with image of the back of the card
           BufferedImage img = null;
           JLabel label = new JLabel();
           try {
-            img = ImageIO.read(new File("Cards/blue_back.png"));
+            if(n==1)
+              img = ImageIO.read(new File("Cards/blue_back.png"));
+            else if(n==2){
+              img = ImageIO.read(new File(png));
+              System.out.println("Show image!!");
+            }
+
 
 
             int height = img.getHeight();
@@ -377,14 +436,23 @@ public class basicsPanel extends JPanel{
 
 
             label.setIcon(imageIcon);
-            if(i == 1) label.setBounds(20, posbot, 200, 200);
+            if(n==1){
+            if(i == 1) label.setBounds(50, posbot, 200, 200);
             else {
               label.setBounds(600,posbot2,200,200);
               posbot2 += 80;
             }
+          }
+          else if(n==2){
+            if(i == 1) label.setBounds(10, posbot, 200, 200);
+            else {
+              label.setBounds(550,posbot2,200,200);
+              posbot2 += 80;
+            }
+          }
 
             posbot += 80;
-
+            //label.setVisible(true);
             add(label);
 
 
@@ -471,7 +539,10 @@ public class basicsPanel extends JPanel{
               first_card = false;
             }
             else{
-                img = ImageIO.read(new File("Cards/blue_back.png"));
+                if(n == 1)
+                  img = ImageIO.read(new File("Cards/blue_back.png"));
+                else if(n==2)
+                  img = ImageIO.read(new File(png));
             }
             //scaling the image to a smaller size and setting it as imageicon for jLabel
             Image dimg = img.getScaledInstance(100, 120, Image.SCALE_SMOOTH);
@@ -494,7 +565,135 @@ public class basicsPanel extends JPanel{
 
 
 
-      }
+}
 
+/*
+      public void showCards(){
+        int cardvalue;
+        String suit;
+        int posbot = 150;
+        int posbot2 = 150;
+
+        //iterating each bot
+        for(int i = 1; i <= 2; i++){
+          cards = game.getBotCards(i);
+          for(Card c : cards){
+          //If the card is not null, then get all the data. (Need this error checking because Card array is initialized with size of 14)
+         if(c != null){
+          cardvalue = c.getcardValue();
+          suit = c.getcardSuit();
+        System.out.println("The bot's card value is: "+cardvalue);
+        System.out.println("The bot's card suit is: "+suit);
+
+          String png = "";                                     //name of the png file
+          String value = String.valueOf(cardvalue);
+
+          //get the card png (easier way and less code...)
+          if(c.getspecCard() == null)
+          {
+            if(suit.equals("Hearts")){
+              png = "Cards/" + value + "H" + ".png";
+            }
+            else if(suit.equals("Spades")){
+              png = "Cards/" + value + "S" + ".png";
+            }
+            else if(suit.equals("Clubs")){
+              png = "Cards/" + value + "C" + ".png";
+            }
+            else if(suit.equals("Diamonds")){
+              png = "Cards/" + value + "D" + ".png";
+            }
+          }
+          else if(c.getspecCard() != null)        // we are checking if we have a face card :)
+          {
+            if(c.getspecCard() == "King"){
+              switch(suit){
+                case "Hearts" : {png = "Cards/" + 13 + "H" + ".png";}
+                case "Clubs"  : {png = "Cards/" + 13 + "C" + ".png";}
+                case "Spades"  : {png = "Cards/" + 13 + "S" + ".png";}
+                case "Diamonds"  : {png = "Cards/" + 13 + "D" + ".png";}
+              }
+            }
+            else if(c.getspecCard() == "Queen"){
+              switch(suit){
+                case "Hearts" : {png = "Cards/" + 12 + "H" + ".png";}
+                case "Clubs"  : {png = "Cards/" + 12 + "C" + ".png";}
+                case "Spades"  : {png = "Cards/" + 12 + "S" + ".png";}
+                case "Diamonds"  : {png = "Cards/" + 12 + "D" + ".png";}
+              }
+            }
+            else if(c.getspecCard() == "Jack"){
+              switch(suit){
+                case "Hearts" : {png = "Cards/" + 11 + "H" + ".png";}
+                case "Clubs"  : {png = "Cards/" + 11 + "C" + ".png";}
+                case "Spades"  : {png = "Cards/" + 11 + "S" + ".png";}
+                case "Diamonds"  : {png = "Cards/" + 11 + "D" + ".png";}
+              }
+            }
+            else if(c.getspecCard() == "Ace"){
+              switch(suit){
+                case "Hearts" : {png = "Cards/" + 1 + "H" + ".png";}
+                case "Clubs"  : {png = "Cards/" + 1 + "C" + ".png";}
+                case "Spades"  : {png = "Cards/" + 1 + "S" + ".png";}
+                case "Diamonds"  : {png = "Cards/" + 1 + "D" + ".png";}
+              }
+            }
+          }
+
+
+
+          //for bot 1 and bot 2, just show the amount of cards with image of the back of the card
+          BufferedImage img = null;
+          JLabel label = new JLabel();
+          try {
+            img = ImageIO.read(new File(png));
+
+
+            int height = img.getHeight();
+            int width = img.getWidth();
+            //rotated image (switching height and width)
+            BufferedImage rotateImg = new BufferedImage(height, width, img.getType());
+            for(int y = 0; y < height; y++){
+              for(int x = 0; x < width; x ++){
+                if(i ==1){
+                  rotateImg.setRGB(y, (width-1)-x, img.getRGB(x, y));
+                }
+              else{
+                rotateImg.setRGB((height-1)-y, x, img.getRGB(x, y));
+              }
+                }
+              }
+
+            //TODO: Rotate the image clockwise for i = 1 and counterclockwise for i = 2
+            //scaling the image to a smaller size and setting it as imageicon for jLabel
+            Image dimg = rotateImg.getScaledInstance(120, 100, Image.SCALE_SMOOTH);
+            ImageIcon imageIcon = new ImageIcon(dimg);
+
+
+
+            label.setIcon(imageIcon);
+            if(i == 1) label.setBounds(20, posbot, 200, 200);
+            else {
+              label.setBounds(600,posbot2,200,200);
+              posbot2 += 80;
+            }
+
+            posbot += 80;
+
+            add(label);
+
+
+          } catch (IOException e) {
+            System.out.println("Error = " + e);
+
+          }
+        } //end of if statement for c != null
+        } //end of for loop
+        }
+
+
+
+      }
+*/
 
 }
